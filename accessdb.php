938a48be532ff64cc3a1dad34d7feb5ea4356340
knightdavieon <43941311+knightdavieon@users.noreply.github.com>
2018-12-06 09:50:57 +0800

    database upload

--- accessdb.php
+++ accessdb.php
@@ -1,9 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$pass = "";
+$servername = "sql167.main-hosting.eu.";
+$username = "u805029426_bons";
+$pass = "1234567890";
+$dbname = "u805029426_bons";
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=bons", $username, $pass);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $pass);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
